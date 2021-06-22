76bdfe5c99ac0dd581ee0f582da95773e81a6d13
hafdhisofien <1114@holbertonschool.com>
2021-06-22 15:38:10 +0100

    string to interface

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -42,29 +42,31 @@ interface Teacher {
   }
 
   interface StudentConstructor {
-    new (firstName: string, lastName: string): StudentClassInterface;
+    new(firstName: string, lastName: string): StudentClassInterface;
   }
-
+  
   interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
     workOnHomework(): string;
     displayName(): string;
   }
-
+  
   export const StudentClass: StudentConstructor =
   class StudentClass implements StudentClassInterface {
-  firstName: string;
-  lastName: string;
-
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+    firstName: string;
+    lastName: string;
+  
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
   }
-
-  workOnHomework(): string {
-    return "Currently working";
-  }
-
-  displayName(): string {
-    return this.firstName;
-  }
-}
