001ac63e17ccf0d3927b0eb5b9cb7968b3dd46c3
hafdhisofien <1114@holbertonschool.com>
2021-06-24 19:34:49 +0100

    update all files

--- 0x01-Webpack/task_1/webpack.config.js
+++ 0x01-Webpack/task_1/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
     path: path.resolve(__dirname, 'public'),
