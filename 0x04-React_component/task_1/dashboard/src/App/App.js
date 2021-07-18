ffe55d71f81f9a45749d398e68179c364dbadf43
hafdhisofien <1114@holbertonschool.com>
2021-07-18 20:01:56 +0100

    all

---
+++ 0x04-React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import "../App/App.css";
+import Notifications from "../Notifications/Notifications";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from "prop-types";
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+  }
+  componentDidMount() {
+    window.addEventListener('keydown', this.logOut);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.logOut);
+  }
+  logOut(e) {
+    if (e.ctrlKey && e.key === 'h') {
+      e.preventDefault();
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+  render() {
+    const { isLoggedIn } = this.props;
+    const listNotifications = this.listNotifications;
+    return (
+      <>
+        <Notifications listNotifications={listNotifications} />
+        <div className="App">
+          <Header />
+          <hr />
+          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <hr />
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => undefined,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+export default App;
